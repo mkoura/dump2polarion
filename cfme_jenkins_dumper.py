c12da58389a714e931a4f5bcf91d5a98700072c5
Martin Kourim <mkourim@redhat.com>
2017-05-30 09:41:00 +0200

    cfme_jenkins_dumper refactoring

--- cfme_jenkins_dumper.py
+++ cfme_jenkins_dumper.py
@@ -4,7 +4,7 @@
 Polarion® dumper for CFME Jenkins
 """
 
-from __future__ import unicode_literals, absolute_import
+from __future__ import unicode_literals, absolute_import, print_function
 
 import sys
 
@@ -15,31 +15,39 @@ def get_testrun_id(version_file):
     """Gets testrun id out of the appliance_version file."""
     with open(version_file) as input_file:
         version = input_file.read().strip()
-    try:
+        version = version.split('-')[0].split('_')[0]
         build_base = version.replace('.', '_')
-        build_last_num = int(build_base.split('_')[3])
-    # pylint: disable=broad-except
-    except Exception:
+    try:
+        zval = int(build_base.split('_')[3])
+    except (IndexError, ValueError):
+        # not in expected format
         return
-    if build_last_num < 10:
+    if zval < 10:
         pad_build = build_base[-1].zfill(2)
         return build_base[:-1] + pad_build
     return build_base
 
 
-def make_args():
-    """Makes args for polarion_dumper."""
+def tweak_args():
+    """Tweaks args for polarion dumper."""
     new_argv = sys.argv[1:]
+
     for index, arg in enumerate(new_argv):
         if arg in ('-t', '--testrun-id'):
             break
     else:
         index = None
-    if index:
-        new_argv[index + 1] = get_testrun_id(new_argv[index + 1])
+
+    testrun_id = get_testrun_id(new_argv[index + 1])
+    if not testrun_id:
+        print("Cannot find testrun id.", file=sys.stderr)
+        sys.exit(1)
+
+    # replace `-t appliance_version` with `-t testrun_id`
+    new_argv[index + 1] = testrun_id
 
     return new_argv
 
 
 if __name__ == '__main__':
-    sys.exit(main(make_args()))
+    sys.exit(main(tweak_args()))
