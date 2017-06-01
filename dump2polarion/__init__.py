b88bbbc7b598190bae4bac2af2b02bb9f9c4aa18
Martin Kourim <mkourim@redhat.com>
2017-06-01 11:46:04 +0200

    configuration loading changes

--- dump2polarion/__init__.py
+++ dump2polarion/__init__.py
@@ -219,34 +219,28 @@ class XunitExport(object):
 
 
 def get_config(config_file=None):
-    """Finds and reads config file and returns its content."""
-    if config_file:
-        # config file specified
-        cflist = (os.path.expanduser(config_file), )
-    else:
-        # find config file in default locations
-        cflist = (
-            os.path.expanduser('~/.config/dump2polarion.yaml'),
-            'dump2polarion.yaml',
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dump2polarion.yaml'))
+    """Loads config file and returns its content."""
+    default_conf = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dump2polarion.yaml')
+    user_conf = config_file or '~/.config/dump2polarion.yaml'
 
-    conf = None
-    for cfile in cflist:
-        try:
-            with open(cfile):
-                conf = cfile
-                break
-        except EnvironmentError:
+    try:
+        with open(os.path.expanduser(user_conf)):
             pass
-
-    if not conf:
+    except EnvironmentError:
+        user_conf = None
         if config_file:
             raise EnvironmentError("cannot open config file '{}'".format(config_file))
-        else:
-            raise EnvironmentError("no config file found")
 
-    with open(conf, 'r') as input_file:
+    with open(default_conf) as input_file:
         config_settings = yaml.load(input_file)
-    logger.info("Config loaded from '{}'".format(conf))
+    logger.debug("Default config loaded from '{}'".format(default_conf))
+
+    if user_conf:
+        with open(user_conf) as input_file:
+            config_settings_user = yaml.load(input_file)
+        logger.info("Config loaded from '{}'".format(user_conf))
+
+        # merge default and user configuration
+        config_settings.update(config_settings_user)
 
     return config_settings
