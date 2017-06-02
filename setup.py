54d63b5125216f3a5d0d19e4f553242cdbee0d6c
Martin Kourim <mkourim@redhat.com>
2017-06-02 14:53:00 +0200

    changes related to pypi upload

--- setup.py
+++ setup.py
@@ -2,18 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='dump2polarion',
-    version='0.3',
+    version='0.4',
     url='https://github.com/mkoura/dump2polarion',
     description='Dump testcases results to xunit file and submit it to Polarion xunit importer',
-    long_description=open('README.md').read().strip(),
+    long_description=open('README.rst').read().strip(),
     author='Martin Kourim',
     author_email='mkourim@redhat.com',
     license='GPL',
     packages=find_packages(exclude=('tests',)),
-    scripts=['csv2sqlite.py', 'polarion_dumper.py', 'cfme_jenkins_dumper.py'],
+    scripts=['csv2sqlite.py', 'polarion_dumper.py'],
     install_requires=['requests', 'pyyaml', 'stomp.py'],
-    keywords='polarion',
-    classifiers=['Private :: Do Not Upload'],  # hack to avoid uploading to pypi
-    include_package_data=True,
-    zip_safe=False
+    keywords=['polarion', 'testing'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'],
+    include_package_data=True
 )
