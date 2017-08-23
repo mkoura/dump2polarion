1762cc30ecf97523d68b088a6ab3795e1bf25c34
Martin Kourim <mkourim@redhat.com>
2017-08-23 16:59:32 +0200

    Store all relevant data from junit-report.xml

--- dump2polarion/junittools.py
+++ dump2polarion/junittools.py
@@ -30,33 +30,37 @@ def import_junit(junit_file, **kwargs):
         if test_data.tag != 'testcase':
             continue
 
-        verdict = 'failed'
+        verdict = None
         comment = ''
         for element in test_data:
-            # we don't want to submit errors and failures anyway
-            if element.tag in ('error', 'failure'):
+            if element.tag == 'error':
+                verdict = 'failed'
+                comment = element.get('message')
+                # continue to see if there's more telling verdict for this record
+            elif element.tag == 'failure':
+                verdict = 'failed'
+                comment = element.get('message')
                 break
             elif element.tag == 'skipped':
+                verdict = 'skipped'
                 comment = element.get('message')
-                if comment:
-                    verdict = 'skipped'
                 break
-        else:
+        if not verdict:
             verdict = 'passed'
-
-        if verdict == 'failed':
-            continue
 
         title = test_data.get('name')
         classname = test_data.get('classname')
         time = test_data.get('time', 0)
+        filepath = test_data.get('file')
 
         record = OrderedDict([
             ('title', title),
             ('classname', classname),
             ('verdict', verdict),
             ('comment', comment),
-            ('time', time)])
+            ('time', time),
+            ('file', filepath),
+        ])
         results.append(record)
 
     return ImportedData(results=results, testrun=None)
