0d991516e886ca95a1d0772c65bf9891de1bd167
Martin Kourim <mkourim@redhat.com>
2017-09-19 12:02:52 +0200

    List all supported python versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dump2polarion',
-    version='0.10',
+    version='0.11',
     url='https://github.com/mkoura/dump2polarion',
     description='Dump testcases results to xunit file and submit it to Polarion xunit importer',
     long_description=open('README.rst').read().strip(),
@@ -15,10 +15,13 @@ setup(
     keywords=['polarion', 'testing'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 2.7'],
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        'Intended Audience :: Developers'],
     include_package_data=True
 )
