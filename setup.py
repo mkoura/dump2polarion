5a5d07c57329145cfdeb7ca58c3e4e0752debe5e
Martin Kourim <mkourim@redhat.com>
2017-11-13 23:04:18 +0100

    Use quotes

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
         'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 2.7',
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers'],
     include_package_data=True
 )
