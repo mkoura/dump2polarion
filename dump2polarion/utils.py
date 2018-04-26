137b57e6dd7a57b8efdddbc7eb56068887ca4ff6
Martin Kourim <mkourim@redhat.com>
2018-04-26 15:13:57 +0200

    Bump version

--- dump2polarion/utils.py
+++ dump2polarion/utils.py
@@ -35,7 +35,7 @@ except ImportError:
 # pylint: disable=invalid-name
 logger = logging.getLogger(__name__)
 
-_VERSION_ID = '21'
+_VERSION_ID = '22'
 _NOT_EXPECTED_FORMAT_MSG = 'XML file is not in expected format'
 
 
