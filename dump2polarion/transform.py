11f2f36a8d1723f7e5e7b3894b26abc40135acc4
Martin Kourim <mkourim@redhat.com>
2018-09-20 14:34:21 +0200

    Insight project configuration

--- dump2polarion/transform.py
+++ dump2polarion/transform.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Functions for validating and transforming results. These are specific for given Polarion project.
+Functions for validating and transforming results. These are specific per Polarion project.
 
 If the 'polarion-lookup-method' is set to 'custom', this is the place where you can
-set the 'id' of the testcase to desired value.
+set the 'id' of the test case to desired value.
 """
 
 from __future__ import absolute_import, unicode_literals
 
 import copy
+import hashlib
 import re
 
 from dump2polarion.verdicts import Verdicts
@@ -75,6 +76,32 @@ def _include_class_in_title(result):
                 result["title"] = "{}.{}".format(last_classname, title)
         # we don't need to pass classnames?
         del result["classname"]
+
+
+def gen_unique_id(string):
+    """Generates unique id out of a string.
+
+    >>> gen_unique_id("vmaas_TestClass.test_name")
+    '5acc5dc795a620c6b4491b681e5da39c'
+    """
+    return hashlib.sha1(string.encode("utf-8")).hexdigest()[:32]
+
+
+def get_testcase_id(testcase, append_str):
+    """Returns new test case ID.
+
+    >>> get_testcase_id({"title": "TestClass.test_name"}, "vmaas_")
+    '5acc5dc795a620c6b4491b681e5da39c'
+    >>> get_testcase_id({"title": "TestClass.test_name", "id": "TestClass.test_name"}, "vmaas_")
+    '5acc5dc795a620c6b4491b681e5da39c'
+    >>> str(get_testcase_id({"title": "TestClass.test_name", "id": "test"}, "vmaas_"))
+    'test'
+    """
+    testcase_title = testcase.get("title")
+    testcase_id = testcase.get("id")
+    if not testcase_id or testcase_title in testcase_id:
+        testcase_id = gen_unique_id("{}{}".format(append_str, testcase_title))
+    return testcase_id
 
 
 def get_xunit_transform_cfme(config):
@@ -150,7 +177,7 @@ def get_xunit_transform_cmp(config):
             # we don't need to pass classnames?
             del result["classname"]
 
-        # if the "test_id" property is present, use it as testcase ID
+        # if the "test_id" property is present, use it as test case ID
         test_id = result.get("test_id", "")
         if test_id:
             result["id"] = test_id
@@ -174,16 +201,82 @@ def get_xunit_transform_cmp(config):
     return results_transform
 
 
+def get_xunit_transform_insights(config):
+    """Return result transformation function for Insights."""
+    skip_searches = ["SKIPME:", "Skipping due to these blockers", "BZ ?[0-9]+", "GH ?#?[0-9]+"]
+    skips = re.compile("(" + ")|(".join(skip_searches) + ")")
+
+    parametrize = config.get("insights_parametrize", False)
+    testcase_append = config.get("insights_testcase_append", "")
+
+    def results_transform(result):
+        """Results transform for Insights."""
+        verdict = result.get("verdict")
+        if not verdict:
+            return None
+
+        result = copy.deepcopy(result)
+
+        _setup_parametrization(result, parametrize)
+        _include_class_in_title(result)
+        _insert_source_info(result)
+
+        result["id"] = get_testcase_id(result, testcase_append)
+
+        verdict = verdict.strip().lower()
+        # we want to submit PASS and WAIT results
+        if verdict in Verdicts.PASS + Verdicts.WAIT:
+            return result
+        comment = result.get("comment")
+        # ... and SKIP results where there is a good reason (blocker etc.)
+        if verdict in Verdicts.SKIP and comment and skips.search(comment):
+            # found reason for skip
+            result["comment"] = comment.replace("SKIPME: ", "").replace("SKIPME", "")
+            return result
+        if verdict in Verdicts.FAIL and comment and "FAILME" in comment:
+            result["comment"] = comment.replace("FAILME: ", "").replace("FAILME", "")
+            return result
+        # we don't want to report this result if here
+        return None
+
+    return results_transform
+
+
 def get_testcases_transform_cfme(config):
-    """Return testcases transformation function for CFME."""
+    """Return test cases transformation function for CFME."""
 
     parametrize = config.get("cfme_parametrize", False)
 
     def testcase_transform(testcase):
-        """Testcases transform for CFME."""
+        """Test cases transform for CFME."""
         testcase = copy.deepcopy(testcase)
 
         _setup_parametrization(testcase, parametrize)
+
+        return testcase
+
+    return testcase_transform
+
+
+def get_testcases_transform_insights(config):
+    """Return test cases transformation function for Insights."""
+
+    parametrize = config.get("insights_parametrize", False)
+    testcase_append = config.get("insights_testcase_append", "")
+    seen_ids = []
+
+    def testcase_transform(testcase):
+        """Test cases transform for Insights."""
+        testcase = copy.deepcopy(testcase)
+
+        _setup_parametrization(testcase, parametrize)
+
+        testcase_id = get_testcase_id(testcase, testcase_append)
+        testcase["id"] = testcase_id
+
+        if parametrize and testcase_id in seen_ids:
+            return None
+        seen_ids.append(testcase_id)
 
         return testcase
 
@@ -199,6 +292,21 @@ def get_requirements_transform_cfme(config):
 
         if "id" in requirement:
             del requirement["id"]
+
+        return requirement
+
+    return requirement_transform
+
+
+def get_requirements_transform_cloudtp(config):
+    """Return requirement transformation function for CLOUDTP."""
+
+    def requirement_transform(requirement):
+        """Requirements transform for CLOUDTP."""
+        requirement = copy.deepcopy(requirement)
+
+        if "id" in requirement:
+            del requirement["id"]
         # TODO: testing purposes, remove once ready
         if not requirement.get("assignee-id"):
             requirement["assignee-id"] = "mkourim"
@@ -210,17 +318,39 @@ def get_requirements_transform_cfme(config):
     return requirement_transform
 
 
+def get_requirements_transform_insights(config):
+    """Return requirement transformation function for Insights."""
+
+    def requirement_transform(requirement):
+        """Requirements transform for Insights."""
+        requirement = copy.deepcopy(requirement)
+        # TODO: append smtg to requirements title?
+        # Would require to also modify all linked-items in test cases.
+
+        if "id" in requirement:
+            del requirement["id"]
+
+        return requirement
+
+    return requirement_transform
+
+
 PROJECT_MAPPING_XUNIT = {
     "RHCF3": get_xunit_transform_cfme,
     "CMP": get_xunit_transform_cmp,
     "CLOUDTP": get_xunit_transform_cfme,
+    "Insights": get_xunit_transform_insights,
 }
 
-PROJECT_MAPPING_TESTCASES = {"RHCF3": get_testcases_transform_cfme}
+PROJECT_MAPPING_TESTCASES = {
+    "RHCF3": get_testcases_transform_cfme,
+    "Insights": get_testcases_transform_insights,
+}
 
 PROJECT_MAPPING_REQ = {
     "RHCF3": get_requirements_transform_cfme,
-    "CLOUDTP": get_requirements_transform_cfme,
+    "CLOUDTP": get_requirements_transform_cloudtp,
+    "Insights": get_requirements_transform_insights,
 }
 
 
@@ -243,14 +373,14 @@ def get_xunit_transform(config):
 
 
 def get_testcases_transform(config):
-    """Returns results transformation function.
+    """Returns test cases transformation function.
 
     The transformation function is returned by calling corresponding "getter" function.
 
-    This allows customizations of results data according to requirements
+    This allows customizations of test cases data according to requirements
     of the specific project.
 
-    When no results data are returned, this result will be ignored
+    When no test cases data are returned, this test case will be ignored
     and will not be written to the resulting XML.
     """
 
@@ -265,10 +395,10 @@ def get_requirements_transform(config):
 
     The transformation function is returned by calling corresponding "getter" function.
 
-    This allows customizations of results data according to requirements
+    This allows customizations of requirements data according to needs
     of the specific project.
 
-    When no results data are returned, this result will be ignored
+    When no requirements data are returned, this requirement will be ignored
     and will not be written to the resulting XML.
     """
 
