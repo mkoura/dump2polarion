7fced3166ea6fc7d92dc45fe63405e0eef030dec
Martin Kourim <mkourim@redhat.com>
2018-09-20 14:33:56 +0200

    Import all exporters when star-import dump2polarion

--- dump2polarion/__init__.py
+++ dump2polarion/__init__.py
@@ -3,10 +3,18 @@
 Imports.
 """
 
-from dump2polarion.xunit_exporter import XunitExport
-from dump2polarion.importer import do_import
 from dump2polarion.configuration import get_config
+from dump2polarion.importer import do_import
+from dump2polarion.requirements_exporter import RequirementExport
 from dump2polarion.submit import submit_and_verify
+from dump2polarion.testcases_exporter import TestcaseExport
+from dump2polarion.xunit_exporter import XunitExport
 
-
-__all__ = ["XunitExport", "do_import", "get_config", "submit_and_verify"]
+__all__ = [
+    "RequirementExport",
+    "TestcaseExport",
+    "XunitExport",
+    "do_import",
+    "get_config",
+    "submit_and_verify",
+]
