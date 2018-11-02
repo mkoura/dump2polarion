5368c2f0c6730898cbcf74ede0d34102716f2995
Martin Kourim <mkourim@redhat.com>
2018-11-02 11:33:45 +0100

    Logs parsing refactoring

--- dump2polarion/parselogs.py
+++ dump2polarion/parselogs.py
@@ -19,15 +19,6 @@ import re
 
 from dump2polarion.exceptions import Dump2PolarionException
 
-_RESULT_SEARCH = re.compile(r"Work item: '(test_[^']+|[A-Z][^']+)' \(([^)]+)\)$")
-_RESULT_WARN_SEARCH = re.compile(r" '(test_[^']+|[A-Z][^']+)'\.$")
-
-_TESTCASE_SEARCH = re.compile(r" test case '(test_[^']+|[A-Z][^']+)' \(([^)]+)\)")
-_TESTCASE_WARN_SEARCH = _RESULT_WARN_SEARCH
-
-_REQ_SEARCH = re.compile(r" requirement '([a-zA-Z][^']+)' \(([^)/]+)")
-_REQ_WARN_SEARCH = re.compile(r" '([^']+)'\.$")
-
 
 class LogItem(object):
     """Represents one work item record in a log file."""
@@ -58,160 +49,198 @@ class ParsedLog(object):
         return "<ParsedLog {}>".format(self.log_type)
 
 
-def get_result(line):
-    """Gets work item name and id."""
-    res = _RESULT_SEARCH.search(line)
-    try:
-        name, ids = res.group(1), res.group(2)
-    except (AttributeError, IndexError):
-        return None
+class XUnitParser(object):
+    """Parser for XUnit logs."""
 
-    ids = ids.split("/")
-    tc_id = ids[0]
-    try:
-        custom_id = ids[1]
-    except IndexError:
-        custom_id = None
-    return LogItem(name, tc_id, custom_id)
+    RESULT_SEARCH = re.compile(r"Work item: '(test_[^']+|[A-Z][^']+)' \(([^)]+)\)$")
+    RESULT_WARN_SEARCH = re.compile(r" '(test_[^']+|[A-Z][^']+)'\.$")
+    RESULT_WARN_SEARCH_CUSTOM = re.compile(r" '(test_[^']+|[A-Z][^']+)' \(([^)]+)\)\.$")
 
+    def __init__(self, fp, log_file):
+        self.fp = fp
+        self.log_file = log_file
 
-def get_result_warn(line):
-    """Gets work item name of item that was not successfully imported."""
-    res = _RESULT_WARN_SEARCH.search(line)
-    try:
-        return LogItem(res.group(1), None, None)
-    except (AttributeError, IndexError):
-        return None
+    def get_result(self, line):
+        """Gets work item name and id."""
+        res = self.RESULT_SEARCH.search(line)
+        try:
+            name, ids = res.group(1), res.group(2)
+        except (AttributeError, IndexError):
+            return None
 
+        ids = ids.split("/")
+        tc_id = ids[0]
+        try:
+            custom_id = ids[1]
+        except IndexError:
+            custom_id = None
+        return LogItem(name, tc_id, custom_id)
 
-def get_testcase(line):
-    """Gets test case name and id."""
-    res = _TESTCASE_SEARCH.search(line)
-    try:
-        name, ids = res.group(1), res.group(2)
-    except (AttributeError, IndexError):
-        return None
+    def get_result_warn(self, line):
+        """Gets work item name of item that was not successfully imported."""
+        res = self.RESULT_WARN_SEARCH.search(line)
+        try:
+            return LogItem(res.group(1), None, None)
+        except (AttributeError, IndexError):
+            pass
 
-    ids = ids.split("/")
-    tc_id = ids[0]
-    try:
-        custom_id = ids[1]
-    except IndexError:
-        custom_id = None
-    return LogItem(name, tc_id, custom_id)
+        # try again with custom ID
+        res = self.RESULT_WARN_SEARCH_CUSTOM.search(line)
+        try:
+            return LogItem(res.group(1), None, res.group(2))
+        except (AttributeError, IndexError):
+            return None
 
+    def parse(self):
+        """Parse log file produced by the XUnit Iporter."""
+        existing_items = []
+        duplicate_items = []
+        new_items = []
+        for line in self.fp:
+            line = line.strip()
+            if "Work item: " in line:
+                existing_it = self.get_result(line)
+                if existing_it:
+                    existing_items.append(existing_it)
+            elif "Unable to find *unique* work item" in line:
+                duplicate_it = self.get_result_warn(line)
+                if duplicate_it:
+                    duplicate_items.append(duplicate_it)
+            elif "Unable to find work item for" in line:
+                new_it = self.get_result_warn(line)
+                if new_it:
+                    new_items.append(new_it)
 
-def get_testcase_warn(line):
-    """Gets name of test case that was not successfully imported."""
-    res = _TESTCASE_WARN_SEARCH.search(line)
-    try:
-        return LogItem(res.group(1), None, None)
-    except (AttributeError, IndexError):
-        return None
+        outcome = ParsedLog("xunit", new_items, existing_items, duplicate_items)
 
+        if not outcome:
+            raise Dump2PolarionException(
+                "No valid data found in the log file '{}'".format(self.log_file)
+            )
 
-def get_requirement(line):
-    """Gets requirement name and id."""
-    res = _REQ_SEARCH.search(line)
-    try:
-        name, tc_id = res.group(1), res.group(2)
-    except (AttributeError, IndexError):
-        return None
-
-    return LogItem(name, tc_id, None)
-
-
-def get_requirement_warn(line):
-    """Gets name of test case that was not successfully imported."""
-    res = _REQ_WARN_SEARCH.search(line)
-    try:
-        return LogItem(res.group(1), None, None)
-    except (AttributeError, IndexError):
-        return None
-
-
-def parse_xunit(fp, log_file):
-    """Parse log file produced by the XUnit Iporter."""
-    existing_items = []
-    duplicate_items = []
-    new_items = []
-    for line in fp:
-        line = line.strip()
-        if "Work item: " in line:
-            existing_it = get_result(line)
-            if existing_it:
-                existing_items.append(existing_it)
-        elif "Unable to find *unique* work item" in line:
-            duplicate_it = get_result_warn(line)
-            if duplicate_it:
-                duplicate_items.append(duplicate_it)
-        elif "Unable to find work item for" in line:
-            new_it = get_result_warn(line)
-            if new_it:
-                new_items.append(new_it)
-
-    outcome = ParsedLog("xunit", new_items, existing_items, duplicate_items)
-
-    if not outcome:
-        raise Dump2PolarionException("No valid data found in the log file '{}'".format(log_file))
-
-    return outcome
+        return outcome
 
 
-def parse_testcase(fp, log_file):
-    """Parse log file produced by the Test Case Importer."""
-    existing_items = []
-    duplicate_items = []
-    new_items = []
-    for line in fp:
-        line = line.strip()
-        if "Updated test case" in line:
-            existing_it = get_testcase(line)
-            if existing_it:
-                existing_items.append(existing_it)
-        elif "Found multiple work items with the title" in line:
-            duplicate_it = get_testcase_warn(line)
-            if duplicate_it:
-                duplicate_items.append(duplicate_it)
-        elif "Created test case" in line:
-            new_it = get_testcase(line)
-            if new_it:
-                new_items.append(new_it)
+class TestcasesParser(object):
+    """Parser for Testcase logs."""
 
-    outcome = ParsedLog("testcase", new_items, existing_items, duplicate_items)
+    TESTCASE_SEARCH = re.compile(r" test case '(test_[^']+|[A-Z][^']+)' \(([^)]+)\)")
+    TESTCASE_WARN_SEARCH = re.compile(r" '(test_[^']+|[A-Z][^']+)'\.$")
 
-    if not outcome:
-        raise Dump2PolarionException("No valid data found in the log file '{}'".format(log_file))
+    def __init__(self, fp, log_file):
+        self.fp = fp
+        self.log_file = log_file
 
-    return outcome
+    def get_testcase(self, line):
+        """Gets test case name and id."""
+        res = self.TESTCASE_SEARCH.search(line)
+        try:
+            name, ids = res.group(1), res.group(2)
+        except (AttributeError, IndexError):
+            return None
+
+        ids = ids.split("/")
+        tc_id = ids[0]
+        try:
+            custom_id = ids[1]
+        except IndexError:
+            custom_id = None
+        return LogItem(name, tc_id, custom_id)
+
+    def get_testcase_warn(self, line):
+        """Gets name of test case that was not successfully imported."""
+        res = self.TESTCASE_WARN_SEARCH.search(line)
+        try:
+            return LogItem(res.group(1), None, None)
+        except (AttributeError, IndexError):
+            return None
+
+    def parse(self):
+        """Parse log file produced by the Test Case Importer."""
+        existing_items = []
+        duplicate_items = []
+        new_items = []
+        for line in self.fp:
+            line = line.strip()
+            if "Updated test case" in line:
+                existing_it = self.get_testcase(line)
+                if existing_it:
+                    existing_items.append(existing_it)
+            elif "Found multiple work items with the title" in line:
+                duplicate_it = self.get_testcase_warn(line)
+                if duplicate_it:
+                    duplicate_items.append(duplicate_it)
+            elif "Created test case" in line:
+                new_it = self.get_testcase(line)
+                if new_it:
+                    new_items.append(new_it)
+
+        outcome = ParsedLog("testcase", new_items, existing_items, duplicate_items)
+
+        if not outcome:
+            raise Dump2PolarionException(
+                "No valid data found in the log file '{}'".format(self.log_file)
+            )
+
+        return outcome
 
 
-def parse_requirements(fp, log_file):
-    """Parse log file produced by the Requirements Importer."""
-    existing_items = []
-    duplicate_items = []
-    new_items = []
-    for line in fp:
-        line = line.strip()
-        if "Updated requirement" in line:
-            existing_it = get_requirement(line)
-            if existing_it:
-                existing_items.append(existing_it)
-        elif "Found multiple work items with the title" in line:
-            duplicate_it = get_requirement_warn(line)
-            if duplicate_it:
-                duplicate_items.append(duplicate_it)
-        elif "Created requirement" in line:
-            new_it = get_requirement(line)
-            if new_it:
-                new_items.append(new_it)
+class RequirementsParser(object):
+    """Parser for Requirement logs."""
 
-    outcome = ParsedLog("requirement", new_items, existing_items, duplicate_items)
+    REQ_SEARCH = re.compile(r" requirement '([a-zA-Z][^']+)' \(([^)/]+)")
+    REQ_WARN_SEARCH = re.compile(r" '([^']+)'\.$")
 
-    if not outcome:
-        raise Dump2PolarionException("No valid data found in the log file '{}'".format(log_file))
+    def __init__(self, fp, log_file):
+        self.fp = fp
+        self.log_file = log_file
 
-    return outcome
+    def get_requirement(self, line):
+        """Gets requirement name and id."""
+        res = self.REQ_SEARCH.search(line)
+        try:
+            name, tc_id = res.group(1), res.group(2)
+        except (AttributeError, IndexError):
+            return None
+
+        return LogItem(name, tc_id, None)
+
+    def get_requirement_warn(self, line):
+        """Gets name of test case that was not successfully imported."""
+        res = self.REQ_WARN_SEARCH.search(line)
+        try:
+            return LogItem(res.group(1), None, None)
+        except (AttributeError, IndexError):
+            return None
+
+    def parse(self):
+        """Parse log file produced by the Requirements Importer."""
+        existing_items = []
+        duplicate_items = []
+        new_items = []
+        for line in self.fp:
+            line = line.strip()
+            if "Updated requirement" in line:
+                existing_it = self.get_requirement(line)
+                if existing_it:
+                    existing_items.append(existing_it)
+            elif "Found multiple work items with the title" in line:
+                duplicate_it = self.get_requirement_warn(line)
+                if duplicate_it:
+                    duplicate_items.append(duplicate_it)
+            elif "Created requirement" in line:
+                new_it = self.get_requirement(line)
+                if new_it:
+                    new_items.append(new_it)
+
+        outcome = ParsedLog("requirement", new_items, existing_items, duplicate_items)
+
+        if not outcome:
+            raise Dump2PolarionException(
+                "No valid data found in the log file '{}'".format(self.log_file)
+            )
+
+        return outcome
 
 
 def parse(log_file):
@@ -219,17 +248,17 @@ def parse(log_file):
     with io.open(os.path.expanduser(log_file), encoding="utf-8") as input_file:
         for line in input_file:
             if "Starting import of XUnit results" in line:
-                handler = parse_xunit
+                obj = XUnitParser
                 break
             elif "Starting import of test cases" in line:
-                handler = parse_testcase
+                obj = TestcasesParser
                 break
             elif "Starting import of requirements" in line:
-                handler = parse_requirements
+                obj = RequirementsParser
                 break
         else:
             raise Dump2PolarionException(
                 "No valid data found in the log file '{}'".format(log_file)
             )
 
-        return handler(input_file, log_file)
+        return obj(input_file, log_file).parse()
